326b6664c11c8e400cf87d155de759c0b33ed38e
sunruina2 <sunruina@finupgroup.com>
2019-10-17 12:01:19 +0800

    summary遇到问题继续训练

--- train_nets.py
+++ train_nets.py
@@ -43,7 +43,7 @@ def get_parser():
 
 
 if __name__ == '__main__':
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
     # 1. define global parameters
 
     net_depth = 50  # resnet depth, default is 50
@@ -57,9 +57,9 @@ if __name__ == '__main__':
     image_size = [112, 112]  # the image size
     num_output = 85164  # the image size
     tfrecords_file_path = '../train_data'  # path to the output of tfrecords file path
-    summary_path = './auroua_output/summary'  # the summary file save path
-    ckpt_path = './auroua_output/ckpt'  # the ckpt file save path
-    log_file_path = './auroua_output/logs'  # the ckpt file save path
+    summary_path = '../auroua_output/summary'  # the summary file save path
+    ckpt_path = '../auroua_output/ckpt'  # the ckpt file save path
+    log_file_path = '../auroua_output/logs'  # the ckpt file save path
     saver_maxkeep = 100  # tf.train.Saver max keep ckpt files
     buffer_size = 10000  # tf dataset api buffer size ?
     log_device_mapping = False  # show device placement log
@@ -171,8 +171,8 @@ if __name__ == '__main__':
     # 3.13 init all variables
     sess.run(tf.global_variables_initializer())
 
-    # restore_saver = tf.train.Saver()
-    # restore_saver.restore(sess, '/home/aurora/workspaces2018/InsightFace_TF/output/ckpt/InsightFace_iter_1110000.ckpt')
+    restore_saver = tf.train.Saver()
+    restore_saver.restore(sess,  ckpt_path + '/InsightFace_iter_'+'90000'+'.ckpt')
     # 4 begin iteration
     if not os.path.exists(log_file_path):
         os.makedirs(log_file_path)
